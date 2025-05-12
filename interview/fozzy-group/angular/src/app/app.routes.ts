e9c6f45fc000097f8a187f8cc86cb8c937c45293
slava-danilchak <danilchakv@gmail.com>
2025-05-12 21:45:20 +0300

    Generate components

--- interview/fozzy-group/angular/src/app/app.routes.ts
+++ interview/fozzy-group/angular/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FagComponent } from './fag/fag.component';
+import { LoginComponent } from './login/login.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LayoutComponent, children: [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'fag', component: FagComponent }
+    ],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: AccessDeniedComponent },
+  // { path: '', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  // { path: 'dashboard', component: DashboardComponent },
+];
