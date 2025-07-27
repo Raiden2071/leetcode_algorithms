63ea20dbf717fe6d257e65ec866e0f57271292b9
slava-danilchak <danilchakv@gmail.com>
2025-07-27 19:02:22 +0300

    JSBASE: Prototype

--- js-base/prototype/prototype.js
+++ js-base/prototype/prototype.js
@@ -1,10 +1,15 @@
-const  toyota = {
-  model: 'rav4',
-  age: 10,
+const car = {
   getInfo: function () {
     console.log('Model' + this.model, 'Age' + this.age);
   }
 };
+
+const toyota = {
+  model: 'rav4',
+  age: 10,
+};
+
+Object.setPrototypeOf(toyota, car); // не рекомендують використовувати, так як погано для перформансу
 
 const nissan = {
   model: 'teana',
@@ -13,6 +18,9 @@ const nissan = {
     console.log('Model' + this.model, 'Age' + this.age);
   }
 }
+
+Object.setPrototypeOf(nissan, car);
+
 
 toyota.getInfo();
 nissan.getInfo();
